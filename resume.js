a3672a8d9b8c181c891c7e0f7ec2d7764068a93f
Alex Krolak <akrolak@rbi.com>
2020-08-05 08:57:45 -0400

    updating code so we match andys code

--- resume.js
+++ resume.js
@@ -23,7 +23,7 @@ const renderEducationalExperience = educationalExperience => `
   educationalExperience.emoji
 }</span>
       ${educationalExperience.university}<span class="comma">,</span>
-      <span class="light">${educationalExperience.degree}</span>
+      <span class="light">${educationalExperience.school}</span>
     </h3>
     <ul>
       ${educationalExperience.details
@@ -35,20 +35,20 @@ const renderEducationalExperience = educationalExperience => `
 function renderSkills(skill) {
   return `
   <div>
-  <span class="date">${skill.dates}</span>
-  <h3>
-  <span role="img" aria-label="${skill.ariaLabel}">${skill.emoji}</span>
-  ${skill.skillone}<span class="comma">,</span>
-  <span class="light">${skill.skilltwo}</span>
-  </h3>
-  <ul>
-  ${skill.details.map(detail => `<li>${detail}</li>`).join('')}
-  </ul>
+    <span class="date">${skill.dates}</span>
+    <h3>
+      <span role="img" aria-label="${skill.ariaLabel}">${skill.emoji}</span>
+      ${skill.skillone}<span class="comma">,</span>
+      <span class="light">${skill.skilltwo}</span>
+    </h3>
+    <ul>
+    ${skill.details.map(detail => `<li>${detail}</li>`).join('')}
+    </ul>
   </div>`;
 }
 
 const fetchData = async () => {
-  const response = await fetch('/data.json');
+  const response = await fetch('data.json');
   const data = await response.json();
 
   document.title = data.name;
@@ -80,6 +80,33 @@ const fetchData = async () => {
   const linkedInLink = document.querySelector('li:nth-child(4) a');
   linkedInLink.href += data.linkedInHandle;
 
+  const workExperienceHeadingEmojiContainer = document.querySelector(
+    "h2:nth-of-type(1) span[role='img']"
+  );
+  workExperienceHeadingEmojiContainer.textContent =
+    data.headingEmojis.workExperience.character;
+  workExperienceHeadingEmojiContainer.setAttribute(
+    'aria-label',
+    data.headingEmojis.workExperience.ariaLabel
+  );
+  const educationalExperienceHeadingEmojiContainer = document.querySelector(
+    "h2:nth-of-type(2) span[role='img']"
+  );
+  educationalExperienceHeadingEmojiContainer.textContent =
+    data.headingEmojis.educationExperience.character;
+  educationalExperienceHeadingEmojiContainer.setAttribute(
+    'aria-label',
+    data.headingEmojis.educationExperience.ariaLabel
+  );
+  const skillsHeadingEmojiContainer = document.querySelector(
+    "h2:nth-of-type(3) span[role='img']"
+  );
+  skillsHeadingEmojiContainer.textContent = data.headingEmojis.skills.character;
+  skillsHeadingEmojiContainer.setAttribute(
+    'aria-label',
+    data.headingEmojis.skills.ariaLabel
+  );
+
   const workExperienceContainer = document.querySelector('#work-experience');
   const eachWorkExperienceHTML = data.workExperiences.map(renderWorkExperience);
   const allWorkExperiencesHTML = eachWorkExperienceHTML.join('');
@@ -92,7 +119,7 @@ const fetchData = async () => {
   const allEducationalExperiencesHTML = eachEducationalExperienceHTML.join('');
   educationContainer.innerHTML = allEducationalExperiencesHTML;
 
-  const skillContainer = document.querySelector('#skills');
+  const skillContainer = document.querySelector(`#skills`);
   const eachSkillHTML = data.skills.map(renderSkills);
   const allSkillsHTML = eachSkillHTML.join('');
   skillContainer.innerHTML = allSkillsHTML;
